46afad43f4bdb358a6b0bb0dd23582217220bcc0
Mateus <mateusfnogueira@outlook.com>
2020-12-26 16:27:36 -0300

    estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React from 'react';
+import './App.css';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      App React
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return <div className={styles.header}>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login / Sign In</Link>
+    </nav>
+  </div>
+};
+
+export default Header;
