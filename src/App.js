04de97abc3c8f13653be9a55a7a72b7f85e0a064
Mateus <mateusfnogueira@outlook.com>
2020-12-27 21:10:10 -0300

    UserContext correção de bugs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
-import Home from './pages/home/Home';
-import Login from './pages/login/Login';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { UserStorage } from "./UserContext";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      <Footer />
-      </Router>
+      <UserStorage>
+        <Router>
+          <Header />
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          <Footer />
+        </Router>
+      </UserStorage>
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import styles from './Header.module.css'
-import { Link } from 'react-router-dom'
-import { ReactComponent as LogoHome } from '../../assets/dogs.svg'
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as LogoHome } from "../../assets/dogs.svg";
+import { UserContext } from "../../UserContext";
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label="Logo Home">
           <LogoHome />
         </Link>
-        <Link className={styles.login} to="/login">Login / Sign In</Link>
+        {data ? (
+          <Link className={styles.login} to="/profile">
+            {data.name}
+          </Link>
+        ) : (
+            <Link className={styles.login} to="/login">
+              Login / Sign In
+            </Link>
+          )}
       </nav>
       {/* TODO fazer header pos Auth */}
     </header>
